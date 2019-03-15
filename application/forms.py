56d7710195bc4ce850e6226d9a62d814d4fbda0a
yoyuyoppe <borovikao88@gmail.com>
2019-03-15 18:05:56 +0600

    update from 2019-03-15

--- application/forms.py
+++ application/forms.py
@@ -1,10 +1,33 @@
 # -*- coding:utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from application.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Пользователь', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Пользователь', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Подтверждение пароля',
+                              validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(
+                'Пожалуйста, используйте другое имя пользователя')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(
+                'Пожалуйста, используйте другое имя пользователя')
